0b4b7c3d971438133b695b1c3ab26ad37a384420
Oleg <Oleg@DESKTOP-0N4UEG3>
2017-03-14 08:01:35 +0200

    Добавьте файлы проекта.

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.Id = id;
+
+            foreach (var t in db.Books)
+            {
+                if (t.Id == id)
+                {
+                    ViewBag.Name = t.Name;
+                    ViewBag.Author = t.Author;
+                    ViewBag.Price = t.Price;
+                    ViewBag.Remark = t.Remark;
+                }
+            }
+
+            return View();
+        }
+    }
+}
